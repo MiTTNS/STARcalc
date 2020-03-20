8b3fff83e7acb5d45a946f78ed30ccf17ff4bb8f
MiTTNS <32443482+MiTTNS@users.noreply.github.com>
2020-03-20 10:27:26 -0400

    Add files via upload

--- options.js
+++ options.js
@@ -1,19 +1,8 @@
-let page = document.getElementById('buttonDiv');
+let hours = document.getElementById('hours');
 
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-
-    button.style.backgroundColor = item;
-
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log("the color is " + item);
-      })
-    });
-    page.appendChild(button);
-  }
+function constructOptions() {
+  let message = document.createElement('p')
+  message.textContent = "this is where you world edit our open times"
+  hours.appendChild(message)
 }
-constructOptions(kButtonColors);
+constructOptions();
